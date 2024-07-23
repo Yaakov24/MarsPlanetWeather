19bdaae0580a5ae7f1c187e47fc6057ddd09b363
YaakovG <ygoldenbergyo@gmail.com>
2024-07-23 16:11:35 -0400

    configured spaceing properly

--- Program.cs
+++ Program.cs
@@ -6,12 +6,12 @@ namespace MarsPlanetW
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+           
             builder.Services.AddRazorPages();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
+            
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Error");
